7579b35e7abdd6079ce919ad5fb58cd9aa49c232
patrick-tbwfon <hiepmac2020@gmail.com>
2023-10-20 13:47:28 +0700

    modified:   app.py 	modified:   extension.js 	modified:   package-lock.json 	modified:   package.json

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
 from flask import Flask, request, jsonify
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoModel
+from transformers import RobertaTokenizer, T5ForConditionalGeneration
+
 import torch
 app = Flask(__name__)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 models = {
     'summarizeModel': {
-        'model': AutoModel.from_pretrained('codet5p-220m-bimodal',
-                                            trust_remote_code=True).to(device),
-        'tokenizer': AutoTokenizer.from_pretrained('codet5p-220m-bimodal',
-                                                    trust_remote_code=True)
+        # 'model': AutoModel.from_pretrained('codet5p-220m-bimodal',
+        #                                     trust_remote_code=True).to(device),
+        # 'tokenizer': AutoTokenizer.from_pretrained('codet5p-220m-bimodal',
+        #                                             trust_remote_code=True)
+        'model': T5ForConditionalGeneration.from_pretrained('E:/CodeT5/finetuned_summarize_python_codet5_base').to(device),
+        'tokenizer': RobertaTokenizer.from_pretrained('E:/CodeT5/codet5-base')
     },
     'autocompleteModel': {
-        'model': AutoModelForSeq2SeqLM.from_pretrained('codet5p-2b',
-                                                    torch_dtype=torch.float16,
-                                                    trust_remote_code=True).to(device),
-        'tokenizer': AutoTokenizer.from_pretrained('codet5p-2b')
+        # 'model': AutoModelForSeq2SeqLM.from_pretrained('codet5p-2b',
+        #                                             torch_dtype=torch.float16,
+        #                                             trust_remote_code=True).to(device),
+        # 'tokenizer': AutoTokenizer.from_pretrained('codet5p-2b')
+        'model': T5ForConditionalGeneration.from_pretrained('E:/CodeT5/codet5-base').to(device),
+        'tokenizer': RobertaTokenizer.from_pretrained('E:/CodeT5/codet5-base')
     },
     'autocompleteModelPython': {
-        'model': AutoModelForSeq2SeqLM.from_pretrained('codet5p-770m-py',
-                                                    trust_remote_code=True.to(device)),
-        'tokenizer': AutoTokenizer.from_pretrained('codet5p-770m-py')
+        # 'model': AutoModelForSeq2SeqLM.from_pretrained('codet5p-770m-py',
+        #                                             trust_remote_code=True.to(device)),
+        # 'tokenizer': AutoTokenizer.from_pretrained('codet5p-770m-py')
+        'model': T5ForConditionalGeneration.from_pretrained('E:/CodeT5/codet5-base').to(device),
+        'tokenizer': RobertaTokenizer.from_pretrained('E:/CodeT5/codet5-base')
     },
 }
 
@@ -46,4 +54,4 @@ def run_model(model_name, max_length):
     return jsonify({'result': decoded_output})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
