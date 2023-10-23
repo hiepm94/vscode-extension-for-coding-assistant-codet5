fb810dbfa8a73822f730a2941f61e104221c67e8
patrick-tbwfon <hiepmac2020@gmail.com>
2023-10-23 16:14:18 +0700

    modified:   README.md 	modified:   extension.js 	modified:   package.json 	deleted:    vsc-extension-quickstart.md

--- extension.js
+++ extension.js
@@ -7,46 +7,115 @@ function activate(context) {
     const selectedText = editor.document.getText(editor.selection);
 
     if (selectedText) {
-      const actions = ['Summarize', 'Autocomplete'];
-
-      vscode.window.showQuickPick(actions)
+      const actions = ['CodeT5+: Code Summarization', 'CodeT5+: Code Autocompletion','CodeT5+: Code Autocompletion (Python specific)',
+                        'CodeT5+: Text-to-code Generatation', 'CodeT5+: Text-to-code Generatation (Python specific)']
+      const options = {
+        placeHolder: 'Select an action'
+      };
+      vscode.window.showQuickPick(actions, options)
         .then(async selectedAction => {
-          if (selectedAction === 'Summarize') {
-            try {
-              const response = await axios.post('http://127.0.0.1:5000/api/summarizeModel', {
-                text: selectedText
-              });
-
-              const summarizedCode = response.data.result;
-
-              // Insert the summarized code before the selected text
-              const insertLine = editor.selection.start.line;
-              const insertPosition = new vscode.Position(insertLine, 0);
-              editor.edit(editBuilder => {
-                editBuilder.insert(insertPosition, '#  ' + summarizedCode + '\n');
-              });
-
-            } catch (error) {
+          switch (selectedAction) {
+            case 'CodeT5+: Code Summarization':
+              try {
+                const response = await axios.post('http://127.0.0.1:5000/api/summarizeModel', {
+                  text: selectedText
+                });
+          
+                const summarizedCode = response.data.result;
+          
+                // Insert the summarized code before the selected text
+                const insertLine = editor.selection.start.line;
+                const insertPosition = new vscode.Position(insertLine, 0);
+                editor.edit(editBuilder => {
+                  editBuilder.insert(insertPosition, '#  ' + summarizedCode + '\n');
+                });
+          
+              } catch (error) {
+                vscode.window.showErrorMessage('Error!');
+              }
+              break;
+          
+            case 'CodeT5+: Code Autocompletion':
+              try {
+                const response = await axios.post('http://127.0.0.1:5000/api/autocompleteModel', {
+                  text: selectedText
+                });
+          
+                const completedCode = response.data.result;
+          
+                // Insert the completed code in the next line of the selected text
+                const insertLine = editor.selection.end.line + 1;
+                const insertPosition = new vscode.Position(insertLine, 0);
+                editor.edit(editBuilder => {
+                  editBuilder.insert(insertPosition, '\n' + completedCode);
+                });
+          
+              } catch (error) {
+                vscode.window.showErrorMessage('Error!');
+              }
+              break;
+            
+            case 'CodeT5+: Code Autocompletion (Python specific)':
+              try {
+                const response = await axios.post('http://127.0.0.1:5000/api/autocompleteModelPython', {
+                  text: selectedText
+                });
+          
+                const completedCode = response.data.result;
+          
+                // Insert the completed code in the next line of the selected text
+                const insertLine = editor.selection.end.line + 1;
+                const insertPosition = new vscode.Position(insertLine, 0);
+                editor.edit(editBuilder => {
+                  editBuilder.insert(insertPosition, '\n' + completedCode);
+                });
+          
+              } catch (error) {
+                vscode.window.showErrorMessage('Error!');
+              }
+              break;
+            
+            case 'CodeT5+: Text-to-code Generatation':
+              try {
+                const response = await axios.post('http://127.0.0.1:5000/api/gencodeModel', {
+                  text: selectedText
+                });
+          
+                const completedCode = response.data.result;
+          
+                // Insert the completed code in the next line of the selected text
+                const insertLine = editor.selection.end.line + 1;
+                const insertPosition = new vscode.Position(insertLine, 0);
+                editor.edit(editBuilder => {
+                  editBuilder.insert(insertPosition, '\n' + completedCode);
+                });
+          
+              } catch (error) {
+                vscode.window.showErrorMessage('Error!');
+              }
+              break;
+            case 'CodeT5+: Text-to-code Generatation (Python specific)':
+              try {
+                const response = await axios.post('http://127.0.0.1:5000/api/gencodeModelPython', {
+                  text: selectedText
+                });
+          
+                const completedCode = response.data.result;
+          
+                // Insert the completed code in the next line of the selected text
+                const insertLine = editor.selection.end.line + 1;
+                const insertPosition = new vscode.Position(insertLine, 0);
+                editor.edit(editBuilder => {
+                  editBuilder.insert(insertPosition, '\n' + completedCode);
+                });
+          
+              } catch (error) {
+                vscode.window.showErrorMessage('Error!');
+              }
+              break;
+            default:
               vscode.window.showErrorMessage('Error!');
-            }
-          } else if (selectedAction === 'Autocomplete') {
-            try {
-              const response = await axios.post('http://127.0.0.1:5000/api/autocompleteModelPython', {
-                text: selectedText
-              });
-
-              const completedCode = response.data.result;
-
-              // Insert the completed code in the next line of the selected text
-              const insertLine = editor.selection.end.line + 1;
-              const insertPosition = new vscode.Position(insertLine, 0);
-              editor.edit(editBuilder => {
-                editBuilder.insert(insertPosition, '\n' + completedCode);
-              });
-
-            } catch (error) {
-              vscode.window.showErrorMessage('Error!');
-            }
+              break;
           }
         });
     }
@@ -61,73 +130,3 @@ module.exports = {
   activate,
   deactivate
 };
-
-
-// 	let disposableSummarize = vscode.commands.registerCommand('codet5-.summarizeCode', async () => {
-// 		const editor = vscode.window.activeTextEditor;
-// 		const selectedText = editor.document.getText(editor.selection);
-	  
-// 		try {
-// 			const response = await axios.post('http://127.0.0.1:5000/api/summarizeModel', {
-// 				text: selectedText
-// 			});
-		
-// 			const summarizedCode = response.data.result;
-	  
-// 			// Insert the completed code before the selected text
-// 			const insertLine = editor.selection.start.line;
-// 			const insertPosition = new vscode.Position(insertLine, 0);
-// 			editor.edit(editBuilder => {
-// 			editBuilder.insert(insertPosition, '#  '+ summarizedCode + '\n');
-// 			});
-	  
-// 		} catch (error) {
-// 		  vscode.window.showErrorMessage('Error!');
-// 		}
-// 	  });
-//   	let disposableAutocomplete = vscode.commands.registerCommand('codet5-.autocompleteCode', async () => {
-// 		const editor = vscode.window.activeTextEditor;
-// 		const selectedText = editor.document.getText(editor.selection);
-	
-// 		try {
-// 		const response = await axios.post('http://127.0.0.1:5000/api/autocompleteModel', {
-// 			text: selectedText
-// 		});
-	
-// 		const completedCode = response.data.result;
-	
-// 		// Insert the completed code in the next line of the selected text
-// 		const insertLine = editor.selection.end.line + 1;
-// 		const insertPosition = new vscode.Position(insertLine, 0);
-// 		editor.edit(editBuilder => {
-// 			editBuilder.insert(insertPosition, '\n' + completedCode);
-// 		});
-	
-// 		} catch (error) {
-// 		vscode.window.showErrorMessage('Error!');
-// 		}
-// 	});
-// 	let disposableAutocompletePython = vscode.commands.registerCommand('codet5-.autocompleteCodePython', async () => {
-// 		const editor = vscode.window.activeTextEditor;
-// 		const selectedText = editor.document.getText(editor.selection);
-	
-// 		try {
-// 		const response = await axios.post('http://127.0.0.1:5000/api/autocompleteModelPython', {
-// 			text: selectedText
-// 		});
-	
-// 		const completedCode = response.data.result;
-	
-// 		// Insert the completed code in the next line of the selected text
-// 		const insertLine = editor.selection.end.line + 1;
-// 		const insertPosition = new vscode.Position(insertLine, 0);
-// 		editor.edit(editBuilder => {
-// 			editBuilder.insert(insertPosition, '\n' + completedCode);
-// 		});
-	
-// 		} catch (error) {
-// 		vscode.window.showErrorMessage('Error!');
-// 		}
-// 	});
-//   context.subscriptions.push(disposableSummarize, disposableAutocomplete, disposableAutocompletePython);
-// }
